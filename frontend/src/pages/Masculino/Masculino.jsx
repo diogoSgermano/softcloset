7770b6f95e0a5c5097f8b59ea95e231966e2e2ec
Diogo Soares Germano <diogogermano28@gmail.com>
2026-05-23 23:07:38 -0300

    chore: create Masculino

---
+++ frontend/src/pages/Masculino/Masculino.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Masculino.module.css';
+
+export default function Masculino() {
+  return (
+    <div className={styles.container}>
+      <h1>Moda Masculina</h1>
+    </div>
+  );
+}
