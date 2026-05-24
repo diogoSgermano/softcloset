c69981a17ded606a1b5f51f225cdfa5fe305a21c
Diogo Soares Germano <diogogermano28@gmail.com>
2026-05-23 23:06:27 -0300

    chore: Create Feminino

---
+++ frontend/src/pages/Feminino/Feminino.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Feminino.module.css';
+
+export default function Feminino() {
+  return (
+    <div className={styles.container}>
+      <h1>Moda Feminina</h1>
+    </div>
+  );
+}
