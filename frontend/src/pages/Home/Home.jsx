b2c6b6705cf34c49f96a32a1f8785f97a69cbe02
Diogo Soares Germano <diogogermano28@gmail.com>
2026-05-24 18:52:16 -0300

    chore: add Navbar, Container and Footer in Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Navbar from '../../layouts/Navbar/Navbar';
+import Footer from '../../layouts/Footer/Footer';
+import Container from '../../layouts/Container/Container';
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <Navbar />
+      <Container>
+        <h1>Home</h1>
+      </Container>
+      <Footer />
+    </>
   );
 }
