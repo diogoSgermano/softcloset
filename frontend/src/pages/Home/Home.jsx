6c2df9bb380a412414c993a8fcfd9ec47169c621
Diogo Soares Germano <diogogermano28@gmail.com>
2026-05-25 19:31:51 -0300

    feat: add section and background image of Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import Navbar from '../../layouts/Navbar/Navbar';
 import Footer from '../../layouts/Footer/Footer';
 import Container from '../../layouts/Container/Container';
-
+import img_background from '../../assets/images/pagina-inicial-background.jpg'
+import styles from './Home.module.css'
 export default function Home() {
   return (
     <>
       <Navbar />
       <Container>
-        <h1>Home</h1>
+        <section className={styles.section}>
+            <h1>ESTILO QUE VESTE VOCÊ</h1>
+            <h3>Encontre as melhores peças para todas as ocasiões</h3>
+            <img className={styles.img} src={img_background} alt="foto introdutória"></img>
+        </section>
       </Container>
       <Footer />
     </>
