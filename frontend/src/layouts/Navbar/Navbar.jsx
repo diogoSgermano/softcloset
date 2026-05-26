e0b98df8a5afc36b9e91e468fd717def2520184e
Diogo Soares Germano <diogogermano28@gmail.com>
2026-05-26 13:17:54 -0300

    chore: add Link Button of logo and items of navbar

--- frontend/src/layouts/Navbar/Navbar.jsx
+++ frontend/src/layouts/Navbar/Navbar.jsx
@@ -2,16 +2,30 @@ import React from 'react';
 import styles from './Navbar.module.css';
 import logo from '../../assets/images/logo-softcloset-v2.png'
 import Container from '../Container/Container';
+import LinkButton from '../../components/LinkButton/LinkButton.jsx'
 
-export default function Navbar({}) {
+export default function Navbar() {
   return (
     <nav>
-      <Container>
         <div className={styles.navbar}>
-          <img className={styles.logoNavbar} src={logo} alt="logo SoftCloset" />
-          
+          <div className={styles.logoContainer}>
+            <LinkButton to="/" variant="transparent">
+              <img className={styles.logoNavbar} src={logo} alt="logo SoftCloset" />
+            </LinkButton>
+          </div>
+
+          <ul className={styles.navLinks}>
+            <li>
+                <LinkButton to="/masculino" variant="transparent">Masculino</LinkButton>
+            </li>
+            <li>
+              <LinkButton to="/feminino" variant="transparent">Feminino</LinkButton>
+            </li>
+          </ul>
+
+          <div className={styles.spacer}></div>
         </div>
-      </Container>
+
     </nav>
   );
 }
