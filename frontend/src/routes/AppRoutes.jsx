dda528e50a575f724feb612253557b59e8faac78
Diogo Soares Germano <diogogermano28@gmail.com>
2026-05-29 12:32:46 -0300

    chore: add route for lancamentos e ofertas

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Masculino from '../pages/Masculino/Masculino';
 import Feminino from '../pages/Feminino/Feminino';
+import Ofertas from '../pages/Ofertas/Ofertas.jsx';
+import Lancamento from '../pages/Lancamento/Lancamento.jsx'
+
 
 export default function AppRoutes() {
   return (
@@ -11,6 +14,9 @@ export default function AppRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/masculino" element={<Masculino />} />
         <Route path="/feminino" element={<Feminino />} />
+        <Route path="/ofertas" element={<Ofertas />} />
+        <Route path="/lancamento" element={<Lancamento />} />
+        
       </Routes>
     </Router>
   );
