b131c9456c00f985b1493f9f0502aec37dd30c77
Diogo Soares Germano <diogogermano28@gmail.com>
2026-05-29 12:31:53 -0300

    chore: create initial Lancamento and Ofertas

---
+++ frontend/src/pages/Lancamento/Lancamento.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Lancamento.module.css';
+
+const Lancamento = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Lançamentos</h1>
+      <p>Fique por dentro das últimas novidades da nossa coleção.</p>
+    </div>
+  );
+};
+
+export default Lancamento;
---
+++ frontend/src/pages/Ofertas/Ofertas.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Ofertas.module.css';
+
+const Ofertas = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Ofertas</h1>
+      <p>Confira as melhores promoções da SoftCloset.</p>
+    </div>
+  );
+};
+
+export default Ofertas;
